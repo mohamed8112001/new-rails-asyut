fd1282868c4ece23108021f50b80b436a99c6f41
mohamed8112001 <mohamedmustafa8112001@gmail.com>
2025-05-27 23:00:56 +0300

    update relation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,4 @@
-# class Post < ApplicationRecord
-#     belongs_to :user
-# end
-
-# class Post < ApplicationRecord
-#     belongs_to :user
-#   end
-
-
   class Post < ApplicationRecord
-    belongs_to :user
+    belongs_to :author , class_name:"User" , foreign_key:"user_id"
   end
   
